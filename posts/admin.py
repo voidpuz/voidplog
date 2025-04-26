24d3da1f2bf5cdb443e26936fc1ae3b1500d5387
voidpuz <voidpointer07@gmail.com>
2025-04-26 20:06:56 +0500

    add: media file support

--- posts/admin.py
+++ posts/admin.py
@@ -6,6 +6,7 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'owner__username', 'created_at', 'updated_at')
+    list_display_links = ("id", "title", "owner__username")
     list_filter = ('created_at', 'updated_at')
     search_fields = ('id', 'title', 'description')
     readonly_fields = ('views_count',)
